6c751f08ea7a7416a43507ea1cc4bcd3941c8236
abhashsolanki <abhashsolanki18@gmail.com>
2024-05-18 14:40:15 +0530

    4

--- backend/mongo.js
+++ backend/mongo.js
@@ -3,40 +3,29 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const uri = "mongodb+srv://abhashsolanki:RkQT4bz7rlclROfs@mern.t4kt5kr.mongodb.net/?retryWrites=true&w=majority&appName=MERN";
+const uri = process.env.MONGO_URI;
 
-if (!uri) {
-  console.error("Error: MONGO_URI is not defined in environment variables");
-  process.exit(1); // Exit process with failure
-}
-
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 5000,  // Timeout after 5s if server is not found
-  socketTimeoutMS: 45000,  // Close sockets after 45s of inactivity
-})
+mongoose.connect(uri)
 .then(() => {
-  console.log("MongoDB connected");
+    console.log("MongoDB connected");
 })
 .catch((error) => {
-  console.error("Failed to connect to MongoDB", error);
-  process.exit(1); // Exit process with failure
+    console.error("Failed to connect to MongoDB", error);
 });
 
 const newSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  name: {
-    type: String,
-    required: true
-  }
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    }
 });
 
 const collection = mongoose.model("collection", newSchema);
